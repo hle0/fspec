3a1c239feca43efe0ca1d9186e11370e9efaff71
hle0 <91701075+hle0@users.noreply.github.com>
2022-07-11 11:44:30 -0400

    add skeleton

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <kiss_fft.h>
+#include <portaudio.h>
+#include <sndfile.h>
+#include <SDL2/SDL.h>
+#include <stdexcept>
+
+class App {
+    public:
+        App() {
+            auto err = Pa_Initialize();
+            if (err != paNoError) {
+                printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+
+                throw std::runtime_error("PortAudio init error");
+            }
+
+            if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+                puts("Error initializing SDL");
+
+                throw std::runtime_error("SDL init error");
+            }
+        }
+
+        void run() {
+            puts("Starting app...");
+            // TODO
+        }
+
+        ~App() {
+            SDL_Quit();
+            Pa_Terminate();
+        }
+};
+
+int main(int argc, char **argv) {
+    App app;
+
+    app.run();
+}
